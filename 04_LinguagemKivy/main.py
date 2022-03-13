dcd45a99d7df51306b603e20f0fcf8f7e9385ae6
jonas-lucas-duarte <jonaslucasduarte.github@gmail.com>
2022-03-13 13:49:35 -0300

    13.03.2022.1.13.49

--- 04_LinguagemKivy/main.py
+++ 04_LinguagemKivy/main.py
@@ -1,18 +1,14 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+
+# Criando um Widget (que herda de BoxLayout = Widget)
+class Incrementador(BoxLayout):
+    # Funcionalidade em linguagem Kivy
+    pass
 
 class Test(App):
     def build(self):
-        box = BoxLayout(orientation='vertical')
-        button = Button(text='Botão 1', font_size=30, on_release=self.incrementar)
-        self.label = Label(text='1', font_size=30)
-        box.add_widget(button)
-        box.add_widget(self.label)
-        return box
-
-    def incrementar(self, button):
-        self.label.text = str(int(self.label.text) + 1)
+        # Instância do Widget
+        return Incrementador()
 
 Test().run()

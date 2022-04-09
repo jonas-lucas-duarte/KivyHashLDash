72e829fa0045c5487c66d1daebb755ab81d44b5f
jonas-lucas-duarte <jonaslucasduarte.github@gmail.com>
2022-04-09 14:43:10 -0300

    2022.04.09.14.43

--- 05_LayoutsDinamicosEKWARGS/main.py
+++ 05_LayoutsDinamicosEKWARGS/main.py
@@ -1,11 +1,16 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
-class Incrementador(BoxLayout):
-    pass
+class Tarefas(BoxLayout):
+    def __init__(self, tarefas, **kwargs): # keyword arguments
+        # Para não modificar o método construtor padrão, deve-se chamar o super
+        super().__init__(**kwargs)
+        for tarefa in tarefas:
+            self.add_widget(Label(text=tarefa, font_size=30))
 
 class Test(App):
     def build(self):
-        return Incrementador()
+        return Tarefas(['Fazer compras', 'Buscar filho'], orientation='horizontal')
 
 Test().run()
